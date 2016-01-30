b426fd74a16c7829e4927bbb920bce08b088ebad
voskobovich <vitaly@voskobovich.com>
2016-01-30 22:09:16 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,72 @@
+Yii2 Price Formatter
+===
+
+This component converts currency values (prices) between fixed-point representation (int as number of cents) and floating point representation (float as dollars and cents).
+
+[![License](https://poser.pugx.org/voskobovich/yii2-price-formatter/license.svg)](https://packagist.org/packages/voskobovich/yii2-price-formatter)
+[![Latest Stable Version](https://poser.pugx.org/voskobovich/yii2-price-formatter/v/stable.svg)](https://packagist.org/packages/voskobovich/yii2-price-formatter)
+[![Latest Unstable Version](https://poser.pugx.org/voskobovich/yii2-price-formatter/v/unstable.svg)](https://packagist.org/packages/voskobovich/yii2-price-formatter)
+[![Total Downloads](https://poser.pugx.org/voskobovich/yii2-price-formatter/downloads.svg)](https://packagist.org/packages/voskobovich/yii2-price-formatter)
+
+
+Support
+---
+[GutHub issues](https://github.com/voskobovich/yii2-price-formatter/issues).
+
+
+See example
+---
+
+The example of the USD.
+
+Conversion from cents to dollars with currency symbol 
+```
+Yii::$app->priceFormatter->toView(30090); // Result: (string) $300.90
+```  
+Conversion from cents to dollars without currency symbol
+```
+Yii::$app->priceFormatter->toEdit(30090); // Result: (float) 300.90
+```  
+Conversion from dollars to cents
+```
+Yii::$app->priceFormatter->toStore(300.90); // Result: (int) 30090
+```
+
+Installation
+---
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist voskobovich/yii2-price-formatter "~1.0"
+```
+
+or add
+
+```
+"voskobovich/yii2-price-formatter": "~1.0"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+---
+
+Configuration component in your app config file  
+```
+[
+    ...
+    'components' => [
+        'priceFormatter' => [
+            'class' => 'voskobovich\price\components\PriceFormatter',
+//            'currencyCode' => 'USD',
+            'currencyCode' => function($component) {
+                return Yii::$app->user->identity->currency_code;
+            }
+        ]
+    ]
+]
+``
---
+++ src/PriceFormatter.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace voskobovich\price\components;
+
+use Yii;
+use yii\base\Component;
+
+
+/**
+ * Class PriceFormatter
+ * @package voskobovich\price\components
+ */
+class PriceFormatter extends Component
+{
+    /**
+     * Currency code
+     * Must be return the 3-letter ISO 4217 currency code indicating the currency to use.
+     * If null, [[currencyCode]] will be used.
+     * @var null|string|callable
+     */
+    public $currencyCode = null;
+
+    /**
+     * Configuration for the number formatter.
+     * This parameter will be merged with [[numberFormatterOptions]].
+     * @var array
+     */
+    public $options = [];
+
+    /**
+     * Configuration for the number formatter.
+     * This parameter will be merged with [[numberFormatterTextOptions]].
+     * @var array
+     */
+    public $textOptions = [];
+
+    /**
+     * Getting currency code
+     * @return callable|mixed|null|string
+     */
+    protected function currencyCode()
+    {
+        if ($this->currencyCode !== null) {
+            if (is_callable($this->currencyCode)) {
+                return call_user_func($this->currencyCode, $this);
+            } else {
+                return $this->currencyCode;
+            }
+        }
+
+        return Yii::$app->formatter->currencyCode;
+    }
+
+    /**
+     * @param $value
+     * @return float
+     */
+    public function toStore($value)
+    {
+        $value = str_replace(',', '.', $value);
+        $value = preg_replace('/\s+/', '', $value);
+        $value = preg_replace('/\x{00a0}/siu', '', $value);
+
+        return round($value * 100, 0);
+    }
+
+    /**
+     * @param $value
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function toEdit($value)
+    {
+        return $value / 100;
+    }
+
+    /**
+     * @param $value
+     * @param array $options
+     * @param array $textOptions
+     * @return string
+     * @throws \yii\base\InvalidConfigException
+     */
+    public function toView($value, $options = [], $textOptions = [])
+    {
+        if ($options) {
+            $options = $this->options;
+        }
+
+        if ($textOptions) {
+            $textOptions = $this->textOptions;
+        }
+
+        return Yii::$app->formatter->asCurrency($this->toEdit($value), $this->currencyCode(), $options, $textOptions);
+    }
+}
